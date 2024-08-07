22972bd3b1984829761c003927aa856d83f33b48
esteban-vm <estebanvm90@outlook.com>
2024-08-07 12:00:00 -0400

    add conditional checking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { NEXT_PUBLIC_API_URL } from '@/constants'
 import { Images, Navigation } from '@/containers'
 import { ImagesContextProvider } from '@/contexts'
 
 export default async function Home({ searchParams }: { searchParams?: { query?: string } }) {
+  if (!NEXT_PUBLIC_API_URL) return null
+
   const query = searchParams?.query
 
   return (
