6b22f8c1c250f54171e97aaf064df13a90d8362e
esteban-vm <estebanvm90@outlook.com>
2024-08-09 09:50:59 -0400

    add error message

--- src/containers/images.container.tsx
+++ src/containers/images.container.tsx
@@ -1,14 +1,23 @@
 'use client'
 
+import { FaFaceSadTear } from 'react-icons/fa6'
 import { ImageCard, ImageGrid, ImageSlider } from '@/components'
 import { SlidesContextProvider, useImagesContext } from '@/contexts'
 import { Loader } from '@/shared'
 
 export default function Images() {
-  const { images, isLoading } = useImagesContext()
+  const { images, isError, isLoading } = useImagesContext()
 
   if (isLoading) {
     return <Loader cssOverride={{ marginTop: '1.75rem' }} size='4.5rem' />
+  }
+
+  if (isError) {
+    return (
+      <h2 style={{ marginTop: '1.75rem', fontSize: '1.5rem' }}>
+        Something went wrong <FaFaceSadTear style={{ display: 'inline' }} />
+      </h2>
+    )
   }
 
   return (
