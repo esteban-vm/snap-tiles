46e08d5fbdccbef40829d72a0617349efceb9971
esteban-vm <estebanvm90@outlook.com>
2024-08-10 09:25:37 -0400

    refactoring and style tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { NEXT_PUBLIC_API_URL } from '@/constants'
 import { Images, Navigation } from '@/containers'
 import { ImagesContextProvider } from '@/contexts'
 
-export default async function Home({ searchParams }: { searchParams?: { query?: string } }) {
+export default function Home({ searchParams }: { searchParams?: { query?: string } }) {
   if (!NEXT_PUBLIC_API_URL) return null
 
   const query = searchParams?.query
--- src/components/image_slider.tsx
+++ src/components/image_slider.tsx
@@ -1,15 +1,13 @@
 'use client'
 
-import * as Icons from 'react-icons/fa6'
+import * as Icons from 'react-icons/lu'
 import { Lightbox } from 'yet-another-react-lightbox'
 import { Fullscreen, Slideshow, Thumbnails, Zoom } from 'yet-another-react-lightbox/plugins'
 import { useSlidesContext } from '@/contexts'
 import { Loader } from '@/shared'
-import { clsx } from '@/utils'
 
 export default function ImageSlider() {
   const { slides, currentIndex, hide, isOpen } = useSlidesContext()
-  const iconClasses = clsx('transition-all active:scale-90 hover:opacity-70')
 
   return (
     <Lightbox
@@ -19,19 +17,19 @@ export default function ImageSlider() {
       open={isOpen}
       plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
       slides={slides}
-      slideshow={{ delay: 5_000 }}
-      thumbnails={{ borderColor: 'oklch(var(--su))', border: 2, vignette: false }}
+      slideshow={{ delay: 4_000 }}
+      thumbnails={{ borderColor: 'oklch(var(--a))', vignette: false }}
       zoom={{ maxZoomPixelRatio: 2 }}
       render={{
-        iconNext: () => <Icons.FaChevronRight className={clsx(iconClasses, 'size-7 text-success')} />,
-        iconPrev: () => <Icons.FaChevronLeft className={clsx(iconClasses, 'size-7 text-success')} />,
-        iconClose: () => <Icons.FaRectangleXmark className={clsx(iconClasses, 'size-7 text-secondary')} />,
-        iconZoomIn: () => <Icons.FaMagnifyingGlassPlus className={clsx(iconClasses, 'mr-2 size-6')} />,
-        iconZoomOut: () => <Icons.FaMagnifyingGlassMinus className={clsx(iconClasses, 'mr-2 size-6')} />,
-        iconEnterFullscreen: () => <Icons.FaMaximize className={clsx(iconClasses, 'mr-2 size-6 text-warning')} />,
-        iconExitFullscreen: () => <Icons.FaMinimize className={clsx(iconClasses, 'mr-2 size-6 text-warning')} />,
-        iconSlideshowPlay: () => <Icons.FaCirclePlay className={clsx(iconClasses, 'mr-2 size-6 text-info')} />,
-        iconSlideshowPause: () => <Icons.FaCirclePause className={clsx(iconClasses, 'mr-2 size-6 text-info')} />,
+        iconNext: () => <Icons.LuChevronRight className='size-7 text-accent' />,
+        iconPrev: () => <Icons.LuChevronLeft className='size-7 text-accent' />,
+        iconClose: () => <Icons.LuXCircle className='size-6 text-secondary' />,
+        iconZoomIn: () => <Icons.LuZoomIn className='mr-2 size-6' />,
+        iconZoomOut: () => <Icons.LuZoomOut className='mr-2 size-6' />,
+        iconEnterFullscreen: () => <Icons.LuExpand className='mr-2 size-6 text-warning' />,
+        iconExitFullscreen: () => <Icons.LuShrink className='mr-2 size-6 text-warning' />,
+        iconSlideshowPlay: () => <Icons.LuPlayCircle className='mr-2 size-6 text-info' />,
+        iconSlideshowPause: () => <Icons.LuPauseCircle className='mr-2 size-6 text-info' />,
         iconLoading: () => <Loader size='3rem' />,
       }}
     />
